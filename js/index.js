7b09ce4adfbbfa2db05ae989c8003f80de48677e
Eduardo Costa <eduardocostaprofessor@gmail.com>
2020-04-24 03:47:09 -0300

    adiciona menu coca + slide + validação de formulário

--- js/index.js
+++ js/index.js
@@ -38,3 +38,34 @@ $('.carousel').slick({
 const viewer = new Viewer(document.getElementById('images'),{
 
 })
+
+
+function enviarOrcamento() {
+    const nome = document.getElementById('nome').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const telefone = document.getElementById('telefone').value.trim();
+    const cidade = document.getElementById('cidade').value.trim();
+    const mensagem = document.getElementById('mensagem').value.trim();
+
+    if (
+        nome.length === 0 
+        || email.length === 0 
+        || telefone.length === 0 
+        || cidade.length === 0 
+        || mensagem.length === 0 
+    ) {
+        alert('preencha todos os campos corretamente')
+    } else {
+        
+        console.log(nome);
+        console.log(email);
+        console.log(telefone);
+        console.log(cidade);
+        console.log(mensagem);
+        
+        alert('enviando ... só que não!');
+    }
+
+
+
+}
